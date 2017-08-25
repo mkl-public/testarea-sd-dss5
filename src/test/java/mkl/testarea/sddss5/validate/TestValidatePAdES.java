6648c4d8cfb702601fe4c45d452074edf35f549b
Michael Klink <Michael.Klink@exceet.de>
2017-08-25 15:15:08 +0200

    Updated to SD DSS 5.1.RC1; additional PAdES test
    * Now SD DSS version 5.1.RC1 is under test, not 5.0.RC1.
    * Updated LOTL loading to follow up the version change.
    * Added PAdES test file pdf-exceetLegacySigned-ECDSA.pdf which uses
    ECDSA in the main signature and RSASSA-PSS in some certificate
    signatures.

--- src/test/java/mkl/testarea/sddss5/validate/TestValidatePAdES.java
+++ src/test/java/mkl/testarea/sddss5/validate/TestValidatePAdES.java
@@ -7,8 +7,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.function.Predicate;
+import java.security.Security;
+import java.util.function.Consumer;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -40,63 +42,72 @@ import eu.europa.esig.dss.x509.KeyStoreCertificateSource;
 public class TestValidatePAdES
 {
     final static File OUTPUTDIR = new File("target/test-outputs/validate");
-    final static TrustedListsCertificateSource TOTL = new TrustedListsCertificateSource();
+    final static TrustedListsCertificateSource LOTL = new TrustedListsCertificateSource();
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception
     {
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null)
+        {
+            //Security.addProvider(new BouncyCastleProvider());
+            Security.insertProviderAt(new BouncyCastleProvider(), 1);
+        }
         OUTPUTDIR.mkdirs();
 
         // The keystore contains certificates extracted from the OJ
         KeyStoreCertificateSource keyStoreCertificateSource = new KeyStoreCertificateSource(new File("src/test/resources/keystore.p12"), "PKCS12", "dss-password");
         TSLRepository tslRepository = new TSLRepository();
-        tslRepository.setTrustedListsCertificateSource(TOTL);
+        tslRepository.setTrustedListsCertificateSource(LOTL);
         TSLValidationJob job = new TSLValidationJob();
         job.setDataLoader(new CommonsDataLoader());
-        job.setDssKeyStore(keyStoreCertificateSource);
+        job.setOjContentKeyStore(keyStoreCertificateSource);
+        job.setLotlRootSchemeInfoUri("https://ec.europa.eu/information_society/policy/esignature/trusted-list/tl.html");
         job.setLotlUrl("https://ec.europa.eu/information_society/policy/esignature/trusted-list/tl-mp.xml");
+        job.setOjUrl("http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=uriserv:OJ.C_.2016.233.01.0001.01.ENG");
         job.setLotlCode("EU");
         job.setRepository(tslRepository);
         job.refresh();
     }
 
-    @Parameters(name = "{index}: Validate signed PDF `{0}´")
+    @Parameters(name = "{index}: Validate signed PDF {0}")
     public static Object[][] data()
     {
         return new Object[][]
         {
             // Pades LTV verification in iTextSharp throws Public key presented not for certificate signature for root CA certificate
             // http://stackoverflow.com/questions/41548918/pades-ltv-verification-in-itextsharp-throws-public-key-presented-not-for-certifi
-            {"pdfa2b_instance_signed_ltv_sample.pdf", (Predicate<Reports>) TestValidatePAdES::simpleIndicationIndeterminate},
+            {"pdfa2b_instance_signed_ltv_sample.pdf", (Consumer<Reports>) TestValidatePAdES::simpleIndicationIndeterminate},
             // some PDF, Signamus signed, Adobe LTV enabled
-            {"pdf-exceetLegacySigned-ltvEnabled.pdf", (Predicate<Reports>) TestValidatePAdES::simpleIndicationTotalPassed}
+            {"pdf-exceetLegacySigned-ltvEnabled.pdf", (Consumer<Reports>) TestValidatePAdES::simpleIndicationTotalPassed},
+            // some PDF, SLMBC signed, ECDSA signature
+            {"pdf-exceetLegacySigned-ECDSA.pdf", (Consumer<Reports>) TestValidatePAdES::simpleIndicationTotalPassed}
         };
     }
 
-    public static boolean simpleIndicationIndeterminate(Reports reports)
+    public static void simpleIndicationIndeterminate(Reports reports)
     {
         SimpleReport simple = reports.getSimpleReport();
-        return simple.getIndication(simple.getFirstSignatureId()).equals(Indication.INDETERMINATE);
+        Assert.assertEquals("Unexpected simple report indication", Indication.INDETERMINATE, simple.getIndication(simple.getFirstSignatureId()));
     }
 
-    public static boolean simpleIndicationTotalPassed(Reports reports)
+    public static void simpleIndicationTotalPassed(Reports reports)
     {
         SimpleReport simple = reports.getSimpleReport();
-        return simple.getIndication(simple.getFirstSignatureId()).equals(Indication.TOTAL_PASSED);
+        Assert.assertEquals("Unexpected simple report indication", Indication.TOTAL_PASSED, simple.getIndication(simple.getFirstSignatureId()));
     }
 
     @Parameter(0)
     public String pdfResourceName;
 
     @Parameter(1)
-    public Predicate<Reports> reportsAssertion;
+    public Consumer<Reports> reportsAssertion;
 
     @Test
     public void testValidateEuLOTL() throws IOException
     {
         try (   InputStream resource = getClass().getResourceAsStream(pdfResourceName))
         {
-            DSSDocument document = new InMemoryDocument(resource);
+            DSSDocument document = new InMemoryDocument(resource, pdfResourceName);
 
             OnlineOCSPSource ocspSource = new OnlineOCSPSource();
             ocspSource.setDataLoader(new OCSPDataLoader());
@@ -108,7 +119,7 @@ public class TestValidatePAdES
             verifier.setCrlSource(crlSource);
             verifier.setDataLoader(new CommonsDataLoader());
             verifier.setOcspSource(ocspSource);
-            verifier.setTrustedCertSource(TOTL);
+            verifier.setTrustedCertSource(LOTL);
 
             SignedDocumentValidator validator = SignedDocumentValidator.fromDocument(document);
             validator.setCertificateVerifier(verifier);
@@ -118,7 +129,7 @@ public class TestValidatePAdES
             Files.write(new File(OUTPUTDIR, pdfResourceName + "-detailed.xml").toPath(), singleton(reports.getXmlDetailedReport()), Charset.forName("UTF8"));
             Files.write(new File(OUTPUTDIR, pdfResourceName + "-diagnostic.xml").toPath(), singleton(reports.getXmlDiagnosticData()), Charset.forName("UTF8"));
 
-            Assert.assertTrue("Assertion of Reports failed", reportsAssertion.test(reports));
+            reportsAssertion.accept(reports);
         }
     }
 }
