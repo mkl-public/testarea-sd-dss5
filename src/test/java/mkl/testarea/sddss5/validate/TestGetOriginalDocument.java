fc796d438b11541daea25324c8a198e6e55da682
mkl <mkl@wir-sind-cool.org>
2020-08-19 18:49:08 +0200

    Updated for esig DSS 5.7

--- src/test/java/mkl/testarea/sddss5/validate/TestGetOriginalDocument.java
+++ src/test/java/mkl/testarea/sddss5/validate/TestGetOriginalDocument.java
@@ -8,13 +8,13 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import eu.europa.esig.dss.DSSDocument;
-import eu.europa.esig.dss.InMemoryDocument;
+import eu.europa.esig.dss.diagnostic.DiagnosticData;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.InMemoryDocument;
 import eu.europa.esig.dss.validation.AdvancedSignature;
 import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import eu.europa.esig.dss.validation.SignedDocumentValidator;
 import eu.europa.esig.dss.validation.reports.Reports;
-import eu.europa.esig.dss.validation.reports.wrapper.DiagnosticData;
 
 /**
  * @author mkl
